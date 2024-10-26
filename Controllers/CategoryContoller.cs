7dfa1e64de451a0c587cb053b0c80d2a1eac0578
omercsx <omercagris@gmail.com>
2024-10-26 11:34:19 -0400

    Display categories in table format on index view
    Added a model to the index view for displaying category data in a table format. Updated the CategoryController to fetch category data from the database and pass it to the view.

--- Controllers/CategoryContoller.cs
+++ Controllers/CategoryContoller.cs
@@ -1,11 +1,21 @@
+using EComCSharp.Data;
+using EComCSharp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EComCSharp.Controllers;
 
 public class CategoryController : Controller
 {
+    private readonly ApplicationDbContext _db;
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    
     public IActionResult Index()
     {
-        return View();
+        List<Category> objCategoryList = _db.Categories.ToList();
+        
+        return View(objCategoryList);
     }
 }
