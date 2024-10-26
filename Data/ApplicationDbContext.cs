a21aac8b3f591be77e061715099e80b5fd24d6c8
omercsx <omercagris@gmail.com>
2024-10-26 11:10:53 -0400

    Add database connection settings and category model to ApplicationDbContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,11 +1,12 @@
+using EComCSharp.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace EComCSharp.Data;
 
 public class ApplicationDbContext : DbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-    {
-        
-    }
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+    public DbSet<Category> Categories { get; set; }
 }
